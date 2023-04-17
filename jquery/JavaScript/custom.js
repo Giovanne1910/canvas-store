3727af4501d51e91784f09c373bddb669638c88f
Giovanne Rosa <79673334+Giovanne1910@users.noreply.github.com>
2023-04-17 19:36:15 -0300

    Add files via upload

---
+++ jquery/JavaScript/custom.js
@@ -0,0 +1,12 @@
+// intancia jquery e evita confiltos
+
+//jQuery( function($) {
+  $(document).ready(function(){
+
+$('.owl-carousel').owlCarousel();
+
+  let titulos = $('h4') // tag
+  let itens = $('.featured-item') // class
+  let destaques = $('#featured') // Id
+  
+})
